2998e69e0803a39e704da1918a13bc18ad05c459
Abd-Elrahman Elsayed <geek.abdoo@gmail.com>
2024-04-02 22:52:53 +0200

    init

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.scss';
-import App from './App';
-
-document.getElementsByTagName('html')[0].setAttribute('dir', 'rtl');
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
