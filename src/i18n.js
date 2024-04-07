0cf91ac20d9e4a4aa3c807af4a492d630defdc4e
Abd-Elrahman Elsayed <geek.abdoo@gmail.com>
2024-04-07 17:53:21 +0200

    centering tracker

--- src/i18n.js
+++ src/i18n.js
@@ -1,21 +1,27 @@
 import i18next from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import HttpApi from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
+import translationEn from './assets/locales/en/translation.json';
+import translationAr from './assets/locales/ar/translation.json';
+
 i18next
-  .use(HttpApi)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
+    resources: {
+      en: {
+        translation: translationEn,
+      },
+      ar: {
+        translation: translationAr,
+      },
+    },
     supportedLngs: ['en', 'ar'],
     fallbackLng: 'en',
     debug: false,
     detection: {
       order: ['path', 'cookie', 'htmlTag'],
       caches: ['cookie'],
-    },
-    backend: {
-      loadPath: './assets/locales/{{lng}}/translation.json',
     },
   });
 
